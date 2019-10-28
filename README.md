39c9d7bbffdbe13d17170e9569b846550c698375
Dominika Miodowska <dominika.miodowska@ongoing.ch>
2019-10-28 16:12:25 +0100

    Build the navbar with js

--- README.md
+++ README.md
@@ -12,9 +12,9 @@ This is the final project of Landing Page for Udacity.
 The project should have a structure like the one shown below. All files shown must be present and the app must successfully render a home page with clear design and functionality added when index.html is loaded in the browser. No errors should display in console.
 
 ## Technology
-1.HTML
-2.CSS
-3.JavaScript
+1. HTML
+2. CSS
+3. JavaScript
 
 ## Acknowledgements
 
--- js/app.js
+++ js/app.js
@@ -17,69 +17,48 @@
  * Define Global Variables
  * 
 */
-// content for section
-const page_content =[
-    {
-        header:"About",
-        text1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.",
-        text2:"Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non."
-    },
-    {
-        header:"Ebook",
-        text1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.",
-        text2:"Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non."
-    },
-    {
-        header:"Books",
-        text1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.",
-        text2:"Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non."
-    },
-    {
-        header:"Contact",
-        text1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.",
-        text2:"Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non."
-    }
-];
 
-
+const navLink = ["about", "books", "ebooks", "contact"];
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
 
-function getIDFromHeader(str){
-    return str.split(" ").join("").toLowerCase();
-}
+// function getIDFromHeader(str){
+//     return str.split(" ").join("").toLowerCase();
+// }
 
-function createSection(header, text1, text2){
-    return(
-        `<section id="${getIDFromHeader(header)}" data-nav="${header}">
-            <div class="landing__container">
-                <h2>${header}</h2>
-                <p>${text1}</p>
-                <p>${text2}</p>
-            </div>
-        </section>`
-    ); 
-}
 // create navbar items
-function createNavbarList(header){
-    return `<li><a class='menu__link' href='#${getIDFromHeader(header)}'>${header}</a></li>`
-}
+
+    const listStart = document.querySelector('#navbar__list');
+
+    const listAbout = document.createElement('li');
+    const listBooks = document.createElement('li');
+    const listEbooks = document.createElement('li');
+    const listContact = document.createElement('li');
+    
+    listStart.appendChild(listAbout);
+    listStart.appendChild(listBooks);
+    listStart.appendChild(listEbooks);
+    listStart.appendChild(listContact);
+
+    listAbout.innerHTML = "<a class='menu__link' href='#about'>About</a>";
+    listEbooks.innerHTML = "<a class='menu__link' href='#ebooks'>Ebooks</a>";
+    listBooks.innerHTML = "<a class='menu__link' href='#books'>Books</a>";
+    listContact.innerHTML = "<a class='menu__link' href='#contact'>Contact</a>";
+      
+        
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
 */
 const navbar_list = document.getElementById("navbar__list");
-const main_container = document.querySelector("main#container");
 
-// build the nav and section
-page_content.forEach(function(content){
-    navbar_list.innerHTML += createNavbarList(content.header);
-    main_container.innerHTML += createSection(content.header, content.text1, content.text2);
-})
+const menu_links = document.querySelectorAll('.menu__link');
+const sections = document.querySelectorAll('section');
+
 
 /**
  * End Main Functions
@@ -87,24 +66,21 @@ page_content.forEach(function(content){
  * 
 */
 
-const menu_links = document.querySelectorAll('.menu__link');
-const sections = document.querySelectorAll('section');
-
 // Add class 'active' to section when near top of viewport
 
 function changeActiveItem(){
     // total number of sections
     let index  = sections.length
 
-    while(--index && window.scrollY + 50 <sections[index].offsetTop){}
+    while(--index && window.scrollY + 350 < sections[index].offsetTop){}
    
     //remove active class 
     menu_links.forEach((link)=> link.classList.remove('active'));
-    sections.forEach((section) => section.classList.remove('your-active-class'));
+    sections.forEach((section) => section.classList.remove('active-section'));
 
     //add active class to navigation item
     menu_links[index].classList.add('active');
-    sections[index].classList.add('your-active-class');
+    sections[index].classList.add('active-section');
 }
 
 // Scroll to anchor ID using scrollTO event
